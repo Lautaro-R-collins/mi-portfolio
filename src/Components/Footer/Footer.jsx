2b33546d44c9fec1f5fcf852a027b705d891ce5f
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2023-10-05 01:41:12 -0300

    commit

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,23 +1,70 @@
 import React from "react";
 import "./Footer.css";
 import { ThemeContext } from "../../Context/theme";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import InstagramIcon from "@material-ui/icons/Instagram";
 
 export const Footer = () => {
   const [{ themename }] = React.useContext(ThemeContext);
 
   return (
-    <>
-      <div className={"section " + themename}>
-        <a href="/" rel="noreferrer" target="_blank">
-          <div className="footerBox">
-            <h3 className="footer">
-              Made with{" "}
-              <span style={{ color: `red`, background: `transparent` }}>❤</span>{" "}
-              By Shreyas
-            </h3>
+    <div className={"section " + themename}>
+      <div className="footerBox">
+        <div className="footer-social">
+          <h3>Mis Redes</h3>
+          <div className="social-icons">
+            <a
+              href="https://github.com/Lautaro-R-collins"
+              target="_blank"
+              rel="noreferrer"
+              className="github-icon"
+            >
+              <GitHubIcon />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/lautaro-r-collins/"
+              target="_blank"
+              rel="noreferrer"
+              className="linkedin-icon"
+            >
+              <LinkedInIcon />
+            </a>
+            <a
+              href="https://twitter.com/Lautarodevelops"
+              target="_blank"
+              rel="noreferrer"
+              className="twitter-icon"
+            >
+              <TwitterIcon />
+            </a>
+            <a
+              href="https://instagram.com/lautaro.develops?igshid=OGQ5ZDc2ODK2ZA=="
+              target="_blank"
+              rel="noreferrer"
+              className="instagram-icon"
+            >
+              <InstagramIcon />
+            </a>
           </div>
-        </a>
+        </div>
+        {/* Frase */}
+        <div className="footer-phrase">
+          <p>
+            Hecho en{" "}
+            <span role="img" aria-label="React logo">
+              ⚛️
+            </span>{" "}
+            con ayuda de{" "}
+            <span role="img" aria-label="Café">
+              ☕
+            </span>{" "}
+            © 2023 Lautaro Rodriguez Collins. 
+          </p>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
+
