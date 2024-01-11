7288e91b6186b5d7b6c9b5e857d3d29bab76901a
Lautaro Rodriguez Collins <135086443+Lautaro-R-collins@users.noreply.github.com>
2024-01-11 00:55:55 -0300

    cambios

--- src/Components/About/Timeline.jsx
+++ src/Components/About/Timeline.jsx
@@ -18,7 +18,7 @@ export const Timeline = () => {
   const [linecolor, setlinecolor] = React.useState(
     themename === "light" ? "#23283e" : "#fcfcfc"
   );
-
+   
   React.useEffect(() => {
     if (themename === "dark") {
       setlinecolor("#23283e");
@@ -32,110 +32,16 @@ export const Timeline = () => {
         <h2 className="section__title" data-aos="fade-right">
           <Trans i18nKey="title2" components={[<span className="different" key="0" />]} />
         </h2>
-        <VerticalTimeline lineColor={linecolor}>
-          <VerticalTimelineElement
-            date= {"2009 - 2013"}
-            contentStyle={{
-              boxShadow: `var(--shadow)`,
-              border: "3px solid var(--clr-primary)",
-              backgroundColor: `var(--clr-bg)`,
-              color: `var(--clr-fg-alt)`,
-            }} 
-            contentArrowStyle={{
-              borderRight: "10px solid  var(--clr-primary)",
-            }}
-            iconStyle={{
-              border: `3px solid var(--clr-primary)`,
-              backgroundColor: `var(--clr-bg)`,
-              color: `var(--clr-primary)`,
-            }}
-            icon={<SchoolIcon />}
-          >
-            <h3
-              className="vertical-timeline-element-title"
-              data-aos="fade-right"
-            >
-              <Trans i18nKey="lin1" components={[<span className="different" key="0" />]} />
-            </h3>
-            <h4
-              className="vertical-timeline-element-subtitle"
-              data-aos="fade-right"
-            >
-              <Trans i18nKey="lin01" components={[<span className="different" key="0" />]} />
-            </h4>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
-            date={"2015 - 2017"}
-            contentStyle={{
-              boxShadow: `var(--shadow)`,
-              border: "3px solid var(--clr-primary)",
-              backgroundColor: `var(--clr-bg)`,
-              color: `var(--clr-fg-alt)`,
-            }}
-            contentArrowStyle={{
-              borderRight: "10px solid  var(--clr-primary)",
-            }}
-            iconStyle={{
-              border: ` 3px solid var(--clr-primary)`,
-              backgroundColor: `var(--clr-bg)`,
-              color: `var(--clr-primary)`,
-            }}
-            icon={<WorkIcon />}
-          >
-            <h3
-              className="vertical-timeline-element-title"
-              data-aos="fade-right"
-            >
-              <Trans i18nKey="lin2" components={[<span className="different" key="0" />]} />
-            </h3>
-            <h4
-              className="vertical-timeline-element-subtitle"
-              data-aos="fade-right"
-            >
-              Walmart Argentina
-            </h4>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
-            date={"2019 - 2022"}
-            contentStyle={{
-              boxShadow: `var(--shadow)`,
-              border: "3px solid var(--clr-primary)",
-              backgroundColor: `var(--clr-bg)`,
-              color: `var(--clr-fg-alt)`,
-            }}
-            contentArrowStyle={{
-              borderRight: "10px solid  var(--clr-primary)",
-            }}
-            iconStyle={{
-              border: ` 3px solid var(--clr-primary)`,
-              backgroundColor: `var(--clr-bg)`,
-              color: `var(--clr-primary)`,
-            }}
-            icon={<SchoolIcon />}
-          >
-            <h3
-              className="vertical-timeline-element-title"
-              data-aos="fade-right"
-            >
-              <Trans i18nKey="lin3" components={[<span className="different" key="0" />]} />
-            </h3>
-            <h4
-              className="vertical-timeline-element-subtitle"
-              data-aos="fade-right"
-            >
-              <Trans i18nKey="lin03" components={[<span className="different" key="0" />]} />
-            </h4>
-            <p data-aos="fade-right">
-            <Trans i18nKey="lin13" components={[<span className="different" key="0" />]} />
-            </p> 
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
+        <VerticalTimeline lineColor={linecolor} layout="1-column-left">
+
+        <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            date={"2020 - 2022"}
+            date={"August 2023 - Present"}
             contentStyle={{
               boxShadow: `var(--shadow)`,
               border: "3px solid var(--clr-primary)",
               backgroundColor: `var(--clr-bg)`,
+
               color: `var(--clr-fg-alt)`,
             }}
             contentArrowStyle={{
@@ -152,18 +58,48 @@ export const Timeline = () => {
               className="vertical-timeline-element-title"
               data-aos="fade-right"
             >
-              <Trans i18nKey="lin4" components={[<span className="different" key="0" />]} />
+              <Trans i18nKey="lin7" components={[<span className="different" key="0" />]} />
             </h3>
             <h4
               className="vertical-timeline-element-subtitle"
               data-aos="fade-right"
             >
-              <Trans i18nKey="lin04" components={[<span className="different" key="0" />]} />
+              <Trans i18nKey="lin07" components={[<span className="different" key="0" />]} />
             </h4>
             <p data-aos="fade-right">
-             <Trans i18nKey="lin14" components={[<span className="different" key="0" />]} />
+            <Trans i18nKey="lin17" components={[<span className="different" key="0" />]} />
             </p>
           </VerticalTimelineElement>
+
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            contentStyle={{
+              boxShadow: `var(--shadow)`,
+              border: "3px solid var(--clr-primary)",
+              backgroundColor: `var(--clr-bg)`,
+              color: `var(--clr-fg-alt)`,
+            }}
+            contentArrowStyle={{
+              borderRight: "10px solid  var(--clr-primary)",
+            }}
+            iconStyle={{
+              border: ` 3px solid var(--clr-primary)`,
+              backgroundColor: `var(--clr-bg)`,
+              color: `var(--clr-primary)`,
+            }}
+            icon={<MenuBookIcon />}
+          >
+            <h3
+              className="vertical-timeline-element-title"
+              data-aos="fade-right"
+            >
+              <Trans i18nKey="lin6" components={[<span className="different" key="0" />]} />
+            </h3>
+            <p data-aos="fade-right">
+            <Trans i18nKey="lin06" components={[<span className="different" key="0" />]} />
+            </p>
+          </VerticalTimelineElement>
+
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
             date={"2023 - 2025"}
@@ -199,42 +135,14 @@ export const Timeline = () => {
             <Trans i18nKey="lin15" components={[<span className="different" key="0" />]} />
             </p>
           </VerticalTimelineElement>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            contentStyle={{
-              boxShadow: `var(--shadow)`,
-              border: "3px solid var(--clr-primary)",
-              backgroundColor: `var(--clr-bg)`,
-              color: `var(--clr-fg-alt)`,
-            }}
-            contentArrowStyle={{
-              borderRight: "10px solid  var(--clr-primary)",
-            }}
-            iconStyle={{
-              border: ` 3px solid var(--clr-primary)`,
-              backgroundColor: `var(--clr-bg)`,
-              color: `var(--clr-primary)`,
-            }}
-            icon={<MenuBookIcon />}
-          >
-            <h3
-              className="vertical-timeline-element-title"
-              data-aos="fade-right"
-            >
-              <Trans i18nKey="lin6" components={[<span className="different" key="0" />]} />
-            </h3>
-            <p data-aos="fade-right">
-            <Trans i18nKey="lin06" components={[<span className="different" key="0" />]} />
-            </p>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            date={"Agosto 2023 - Actualidad"}
-            contentStyle={{
-              boxShadow: `var(--shadow)`,
-              border: "3px solid var(--clr-primary)",
-              backgroundColor: `var(--clr-bg)`,
 
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date={"2020 - 2022"}
+            contentStyle={{
+              boxShadow: `var(--shadow)`,
+              border: "3px solid var(--clr-primary)",
+              backgroundColor: `var(--clr-bg)`,
               color: `var(--clr-fg-alt)`,
             }}
             contentArrowStyle={{
@@ -251,18 +159,118 @@ export const Timeline = () => {
               className="vertical-timeline-element-title"
               data-aos="fade-right"
             >
-              <Trans i18nKey="lin7" components={[<span className="different" key="0" />]} />
+              <Trans i18nKey="lin4" components={[<span className="different" key="0" />]} />
             </h3>
             <h4
               className="vertical-timeline-element-subtitle"
               data-aos="fade-right"
             >
-              <Trans i18nKey="lin07" components={[<span className="different" key="0" />]} />
+              <Trans i18nKey="lin04" components={[<span className="different" key="0" />]} />
             </h4>
             <p data-aos="fade-right">
-            <Trans i18nKey="lin17" components={[<span className="different" key="0" />]} />
+             <Trans i18nKey="lin14" components={[<span className="different" key="0" />]} />
             </p>
           </VerticalTimelineElement>
+
+          <VerticalTimelineElement
+            date={"2019 - 2022"}
+            contentStyle={{
+              boxShadow: `var(--shadow)`,
+              border: "3px solid var(--clr-primary)",
+              backgroundColor: `var(--clr-bg)`,
+              color: `var(--clr-fg-alt)`,
+            }}
+            contentArrowStyle={{
+              borderRight: "10px solid  var(--clr-primary)",
+            }}
+            iconStyle={{
+              border: ` 3px solid var(--clr-primary)`,
+              backgroundColor: `var(--clr-bg)`,
+              color: `var(--clr-primary)`,
+            }}
+            icon={<SchoolIcon />}
+          >
+            <h3
+              className="vertical-timeline-element-title"
+              data-aos="fade-right"
+            >
+              <Trans i18nKey="lin3" components={[<span className="different" key="0" />]} />
+            </h3>
+            <h4
+              className="vertical-timeline-element-subtitle"
+              data-aos="fade-right"
+            >
+              <Trans i18nKey="lin03" components={[<span className="different" key="0" />]} />
+            </h4>
+            <p data-aos="fade-right">
+            <Trans i18nKey="lin13" components={[<span className="different" key="0" />]} />
+            </p> 
+          </VerticalTimelineElement>
+
+          <VerticalTimelineElement
+            date={"2015 - 2017"}
+            contentStyle={{
+              boxShadow: `var(--shadow)`,
+              border: "3px solid var(--clr-primary)",
+              backgroundColor: `var(--clr-bg)`,
+              color: `var(--clr-fg-alt)`,
+            }}
+            contentArrowStyle={{
+              borderRight: "10px solid  var(--clr-primary)",
+            }}
+            iconStyle={{
+              border: ` 3px solid var(--clr-primary)`,
+              backgroundColor: `var(--clr-bg)`,
+              color: `var(--clr-primary)`,
+            }}
+            icon={<WorkIcon />}
+          >
+            <h3
+              className="vertical-timeline-element-title"
+              data-aos="fade-right"
+            >
+              <Trans i18nKey="lin2" components={[<span className="different" key="0" />]} />
+            </h3>
+            <h4
+              className="vertical-timeline-element-subtitle"
+              data-aos="fade-right"
+            >
+              Walmart Argentina
+            </h4>
+          </VerticalTimelineElement>
+
+          <VerticalTimelineElement  
+            date= {"2009 - 2013"}
+            contentStyle={{
+              boxShadow: `var(--shadow)`,
+              border: "3px solid var(--clr-primary)",
+              backgroundColor: `var(--clr-bg)`,
+              color: `var(--clr-fg-alt)`,
+            }} 
+            contentArrowStyle={{
+              borderRight: "10px solid  var(--clr-primary)",
+            }}
+            iconStyle={{
+              border: `3px solid var(--clr-primary)`,
+              backgroundColor: `var(--clr-bg)`,
+              color: `var(--clr-primary)`,
+            }}
+            icon={<SchoolIcon />}
+          >
+            <h3
+              className="vertical-timeline-element-title"
+              data-aos="fade-right"
+            >
+              <Trans i18nKey="lin1" components={[<span className="different" key="0" />]} />
+            </h3>
+            <h4
+              className="vertical-timeline-element-subtitle"
+              data-aos="fade-right"
+            >
+              <Trans i18nKey="lin01" components={[<span className="different" key="0" />]} />
+            </h4>
+          </VerticalTimelineElement>
+
         </VerticalTimeline>
       </div>
       <GitHubCalendarComponent />
