dd1dc8274dd1e367353e68974feeeccadf4dc451
Lautaro Rodriguez Collins <135086443+Lautaro-R-collins@users.noreply.github.com>
2024-05-10 02:14:45 -0300

    commit

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -12,6 +12,7 @@ import {
   SiExpress,
   SiMongodb,
   SiNodedotjs,
+  SiJavascript,
 } from "react-icons/si";
 import { IoLogoJavascript } from "react-icons/io";
 import { Trans } from 'react-i18next';
@@ -135,7 +136,7 @@ export const Projects = () => {
               <div className="project_information">
                 <h2>Pixel Art</h2>
                 <p>
-                  Aplicacion Web para crear Pixel Art
+                  Entretenida aplicacion Web para crear arte con pixel echa con JS
                 </p>
                 <div>
                   <SiHtml5 />
@@ -159,6 +160,44 @@ export const Projects = () => {
                   >
                     <span type="button" className="btns onbt">
                       {t("vercod")} 
+                    </span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+          
+          <div className="projects_container" data-aos="fade-right">
+            <div className="project">
+              <div className="project_videocontainer">
+                <div>
+                  <img
+                    src="https://pbs.twimg.com/media/GNMQcNCXYAAeBNx?format=jpg&name=small"
+                    alt="Vegna"
+                  />
+                </div>
+              </div>
+              <div className="project_information">
+                <h2>Vegna web page</h2>
+                <p>
+                Desarrollo el front-end de una página web dedicada al comercio electrónico y al desarrollo web.
+                </p>
+                <div>
+                  <FaReact />
+                  <SiVite />
+                  <SiJavascript />                 
+                  <SiTailwindcss /> 
+                </div>
+                <div>
+                  <a
+                    href="https://vegna-tech.onrender.com//"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <span type="button" className="btns onbt">
+                      {t("verpag")} 
                     </span>
                   </a>
                 </div>
@@ -224,7 +263,7 @@ export const Projects = () => {
               <div className="project_information">
                 <h2>weather app</h2>
                 <p>
-                  App del tiempo online 
+                  App web para consultar el tiempo en cualquier cuidad
                 </p>
                 <div>
                   <SiHtml5 />
@@ -254,43 +293,6 @@ export const Projects = () => {
               </div>
             </div>
           </div>
-
-          <div className="projects_container" data-aos="fade-right">
-            <div className="project">
-              <div className="project_videocontainer">
-                <div>
-                  <img
-                    src="https://pbs.twimg.com/media/GIfD35gWsAAyaKu?format=jpg&name=small"
-                    alt="Sk-Weather-App"
-                  />
-                </div>
-              </div>
-              <div className="project_information">
-                <h2>Calculador JS</h2>
-                <p>
-                  App calculadora online 
-                </p>
-                <div>
-                  <SiHtml5 />
-                  <SiCss3 />
-                  <IoLogoJavascript />  
-                </div>
-                <div>
-                  <a
-                    href="https://lautaro-r-collins.github.io/odin-calculator/"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <span type="button" className="btns onbt">
-                      {t("verpag")} 
-                    </span>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-
         </div>
       </div>
     <div className="center-button-container">
